583889a9936db22b600661e62fbc05a77cd74f5b
Alla Varavina <fistawka27@gmail.com>
2024-08-20 22:50:45 +0200

    fix

--- tests/setup/login.setup.ts
+++ tests/setup/login.setup.ts
@@ -1,13 +1,11 @@
 import { test as setup } from '@playwright/test';
 import { HomePage } from '../../src/pages/HomePage';
-
-const USER = process.env.APP_USER_EMAIL!;
-const PASS = process.env.APP_USER_PASS!;
+import { users } from '../../test-data/credential';
 
 setup('Login', async ({ page }) => {
     const homePage = new HomePage(page);
     await homePage.navigate();
-    await homePage.loginAsUser(USER, PASS);
+    await homePage.loginAsUser(users.mainUser.email, users.mainUser.password);
     await page.getByRole('button', {name: 'Add car'}).waitFor();
     await page.context().storageState({path: 'session-storage.json'})
 });
